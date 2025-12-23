b8b867ec74d265a62a5a848105801e5f905b6998
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-12-23 13:48:07 +0100

    Refactor LoginPage class with improved structure

--- src/Pages/LoginPage.php
+++ src/Pages/LoginPage.php
@@ -3,29 +3,55 @@ namespace SauceDemo\Pages;
 
 use Facebook\WebDriver\WebDriver;
 use Facebook\WebDriver\WebDriverBy;
+use Exception;
 
-class LoginPage {
-    private $driver;
-    private $usernameField;
-    private $passwordField;
-    private $loginButton;
+class LoginPage
+{
+    private WebDriver $driver;
 
-    public function __construct(WebDriver $driver) {
+    public function __construct(WebDriver $driver)
+    {
         $this->driver = $driver;
-        // Localizadores por ID
-        $this->usernameField = WebDriverBy::id("user-name");
-        $this->passwordField = WebDriverBy::id("password");
-        $this->loginButton = WebDriverBy::id("login-button");
     }
 
-    public function open() {
-        $this->driver->get("https://www.saucedemo.com/");
+    // localizadores
+    private WebDriverBy $userNameField;
+    private WebDriverBy $passWordField;
+    private WebDriverBy $loginBtn;
+
+    private function initLocators(): void
+    {
+        $this->userNameField = WebDriverBy::id('user-name');
+        $this->passWordField = WebDriverBy::id('password');
+        $this->loginBtn = WebDriverBy::id('login-button');
     }
 
-    public function login($username, $password) {
-        $this->driver->findElement($this->usernameField)->clear(); // Limpieza por seguridad
-        $this->driver->findElement($this->usernameField)->sendKeys($username);
-        $this->driver->findElement($this->passwordField)->sendKeys($password);
-        $this->driver->findElement($this->loginButton)->click();
+    public function openLoginPage(): void
+    {
+        try {
+            $this->driver->get('https://www.saucedemo.com/');
+            $this->initLocators();
+        } catch (Exception $e) {
+            throw new Exception('Impossible to open Login page: ' . $e->getMessage());
+        }
+    }
+
+    public function enterUserName(string $userName): void
+    {
+        $el = $this->driver->findElement($this->userNameField);
+        $el->clear();
+        $el->sendKeys($userName);
+    }
+
+    public function enterPassWord(string $passWord): void
+    {
+        $el = $this->driver->findElement($this->passWordField);
+        $el->clear();
+        $el->sendKeys($passWord);
+    }
+
+    public function clickOnLogin(): void
+    {
+        $this->driver->findElement($this->loginBtn)->click();
     }
 }
